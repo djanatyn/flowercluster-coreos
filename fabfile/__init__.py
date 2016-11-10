eb5e69c2ab59bd213141cefd174c7c6b19d31740
Jonathan Strickland <djanatyn@gmail.com>
2016-11-10 13:29:45 -0500

    we run sshd on port 2222 now

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 from fabric_gce_tools import update_roles_gce
+from fabric.api import env
 
 import vault
 import instances
+
+env.port = '2222'
 
 update_roles_gce(use_cache=False)
 
